4aa1f4dc060b7c62e0e137ed29e1fa35cfca9ce1
Elvis Serrão <elvis.serrao1@gmail.com>
2019-12-09 00:22:31 -0300

    Add list of User's microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -65,6 +66,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      @post = @user.microposts
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
